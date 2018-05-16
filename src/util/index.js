a3bbfd5dc972671fc155ba393ed651db58d349b6
mrowe122 <mrowe122@gmail.com>
2018-05-16 08:23:31 -0400

    reverting back to stable state

--- src/util/index.js
+++ src/util/index.js
@@ -1,4 +1,4 @@
-import { keyBy, assign, flatMap, get, getOr, sum, includes } from 'lodash/fp'
+import { keyBy, assign, flatMap } from 'lodash/fp'
 
 export const mergeEquipment = (data, ids) => new Promise(resolve => {
   const _itemIdKey = keyBy('id')(ids[0].body)
@@ -11,43 +11,5 @@ export const mergeEquipment = (data, ids) => new Promise(resolve => {
       skin: _skinIdKey[skin]
     }
   ))(data.equipment)
-  let defense = 0
-  let Toughness = 0
-  let upgrades = {}
-  _equipment.filter(e =>
-      e.slot !== 'HelmAquatic' &&
-      e.slot !== 'WeaponA1' &&
-      e.slot !== 'WeaponA2' &&
-      e.slot !== 'WeaponB1' &&
-      e.slot !== 'WeaponB2' &&
-      e.slot !== 'WeaponAquaticA' &&
-      e.slot !== 'WeaponAquaticB').map(e => {
-    defense += get('data.details.defense')(e) || 0
-    Toughness += sum(getOr([], 'data.details.infix_upgrade.attributes')(e).map(a => a.attribute === 'Toughness' && a.modifier))
-    getOr([], 'upgrades')(e).map(u => {
-      if (!upgrades[u.id]) {
-        upgrades[u.id] = {
-          count: 1,
-          bonus: u.details.bonuses || []
-        }
-      } else {
-        upgrades[u.id].count++
-      }
-    })
-    Toughness += sum(flatMap(i =>
-      getOr([], 'details.infix_upgrade.attributes')(i).map(a => a.attribute === 'Toughness' && a.modifier)
-    )(getOr([], 'infusions')(e)))
-  })
-  for (let key in upgrades) {
-    console.log(upgrades)
-    for (let i = 0; i < upgrades[key].count; i++) {
-      Toughness += includes('Toughness', upgrades[key].bonus[i]) ? parseInt(upgrades[key].bonus[i]) : 0
-    }
-  }
-
-  console.log(upgrades)
-  console.log('defense', defense)
-  console.log('Toughness', Toughness)
-  console.log(Toughness + defense + 1000)
   resolve(assign(data, { equipment: keyBy('slot')(_equipment) }))
 })
